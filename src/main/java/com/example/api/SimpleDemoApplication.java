cd06e1db8405b1ae33e9c6aaa47af5ceb15dc52c
asaikali <asaikali@gmail.com>
2023-01-11 09:14:23 -0500

    enhance accelerator

--- src/main/java/com/example/api/SimpleDemoApplication.java
+++ src/main/java/com/example/api/SimpleDemoApplication.java
@@ -3,6 +3,8 @@ package com.example.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class SimpleDemoApplication {
     public static void main(String[] args) {
